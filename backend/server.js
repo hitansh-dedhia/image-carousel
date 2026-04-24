48c8134632c08b9471b3e666a0360597c62be9ef
hitansh-dedhia <dedhiahitansh18@gmail.com>
2026-04-24 11:19:25 +0530

    prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ import imageRoutes from "./routes/images.js";
 import cors from "cors";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 
